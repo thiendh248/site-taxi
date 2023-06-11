80a9f8a644779b3c5445efa3fbf23fab886883fa
thiendh248 <thiendh248@gmail.com>
2023-06-11 14:04:45 +0700

    add QR code kakaotalk & whatsapp

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,7 +1,42 @@
-import {Component, OnInit, TemplateRef} from '@angular/core';
+import { Component, Input, OnInit, TemplateRef } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
-// import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgIf } from '@angular/common';
 
+@Component({
+  selector: 'ngbd-modal-content',
+  standalone: true,
+  template: `
+    <div class="modal-header">
+      <button
+        type="button"
+        class="btn-close"
+        aria-label="Close"
+        (click)="activeModal.dismiss('Cross click')"
+      ></button>
+    </div>
+    <div class="modal-body">
+      <img
+        *ngIf="name === 'kakaotalk'"
+        src="../../content/images/QR_kakaotalk.jpg"
+        style="width: 100%"
+        alt=""
+      />
+      <img
+        *ngIf="name === 'whatsapp'"
+        src="../../content/images/QR_whatsapp.jpg"
+        style="width: 100%"
+        alt=""
+      />
+    </div>
+  `,
+  imports: [NgIf],
+})
+export class NgbdModalContent {
+  @Input() name: 'kakaotalk' | 'whatsapp' = 'kakaotalk';
+
+  constructor(public activeModal: NgbActiveModal) {}
+}
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
@@ -9,10 +44,10 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 })
 export class ContainerComponent implements OnInit {
   mobileView: boolean = false;
-  isModalOpen = false;
 
-
-  constructor(private deviceDetector: DeviceDetectorService,
+  constructor(
+    private deviceDetector: DeviceDetectorService,
+    private modalService: NgbModal
   ) {}
   ngOnInit(): void {
     this.mobileView = this.deviceDetector.isMobile();
@@ -20,12 +55,10 @@ export class ContainerComponent implements OnInit {
     banner!.style.backgroundImage = "url('./content/images/banner.jpeg')";
   }
 
-
-  openModal() {
-    this.isModalOpen = true;
-  }
-
-  closeModal() {
-    this.isModalOpen = false;
+  openModal(name: string) {
+    const modalRef = this.modalService.open(NgbdModalContent, {
+      centered: true,
+    });
+    modalRef.componentInstance.name = name;
   }
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbdModalContent } from '../container/container.component';
 
 @Component({
   selector: 'app-footer',
@@ -9,8 +11,18 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 export class FooterComponent implements OnInit {
   mobileView: boolean = false;
 
-  constructor(private deviceDetector: DeviceDetectorService) {}
+  constructor(
+    private deviceDetector: DeviceDetectorService,
+    private modalService: NgbModal
+  ) {}
   ngOnInit(): void {
     this.mobileView = this.deviceDetector.isMobile();
+  }
+
+  openModal(name: string) {
+    const modalRef = this.modalService.open(NgbdModalContent, {
+      centered: true,
+    });
+    modalRef.componentInstance.name = name;
   }
 }
